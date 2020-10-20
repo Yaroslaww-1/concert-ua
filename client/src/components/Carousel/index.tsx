0420c5e6f0461a47c73ea8c5e8a7822a7cd36931
Yaroslaww1 <yaroslaww1@gmail.com>
2020-10-20 17:50:48 +0300

    client: add auto moving to carousel

--- client/src/components/Carousel/index.tsx
+++ client/src/components/Carousel/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useInterval } from 'src/common/hooks/use-interval';
 
 import styles from './styles.module.scss';
 
 interface IProps {
   columns?: number;
   withArrows?: boolean;
-  changeTimeoutInSec?: number;
+  changeTimeout?: number;
   items: JSX.Element[];
   className?: string;
 }
@@ -13,7 +14,7 @@ interface IProps {
 const Carousel: React.FC<IProps> = ({
   columns = 1,
   withArrows = true,
-  changeTimeoutInSec = 5,
+  changeTimeout = 5000,
   className = '',
   items: propsItems,
 }) => {
@@ -21,6 +22,31 @@ const Carousel: React.FC<IProps> = ({
   const [visibleItemIndexes, setVisibleItemIndexes] = React.useState<number[]>(
     items.slice(0, columns).map((item) => item.index),
   );
+
+  React.useEffect(() => {
+    setVisibleItemIndexes(items.slice(0, columns).map((item) => item.index));
+  }, [columns, propsItems]);
+
+  const moveRight = () => {
+    const lastVisibleIndex = visibleItemIndexes[visibleItemIndexes.length - 1];
+    const [, ...visibleIndexes] = visibleItemIndexes;
+    const newVisibleIndex = lastVisibleIndex + 1 < items.length ? lastVisibleIndex + 1 : 0;
+    visibleIndexes.push(newVisibleIndex);
+    setVisibleItemIndexes(visibleIndexes);
+  };
+
+  const moveLeft = () => {
+    const visibleIndexes = visibleItemIndexes;
+    const firstVisibleIndex = visibleIndexes[0];
+    visibleIndexes.pop();
+    const newVisibleIndex = firstVisibleIndex - 1 >= 0 ? firstVisibleIndex - 1 : items.length - 1;
+    setVisibleItemIndexes([newVisibleIndex, ...visibleIndexes]);
+  };
+
+  useInterval(() => {
+    moveRight();
+  }, changeTimeout);
+
   return (
     <div className={`${styles.wrapper} ${className}`}>
       {items.filter((item) => visibleItemIndexes.includes(item.index)).map((item) => item.item)}
