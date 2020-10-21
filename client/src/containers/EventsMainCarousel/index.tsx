b45a219f4a2c46f3eb0cb64b448d679aaa1bf3f6
Yaroslaww1 <yaroslaww1@gmail.com>
2020-10-21 20:19:31 +0300

    client: add dots navigation

--- client/src/containers/EventsMainCarousel/index.tsx
+++ client/src/containers/EventsMainCarousel/index.tsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'src/redux/rootReducer';
 
 import styles from './styles.module.scss';
-import Carousel from 'src/components/Carousel';
 import Slider from 'react-slick';
 
 const EventsMainCarousel: React.FC = () => {
@@ -15,32 +14,17 @@ const EventsMainCarousel: React.FC = () => {
   } = useSelector((state: RootState) => ({ home: state.home }));
   return (
     <div className={styles.carouselWrapper}>
-      {/* <Carousel
-        className={styles.carouselContent}
-        leftArrowClassName={styles.arrowLeft}
-        rightArrowClassName={styles.arrowRight}
-        navigationClassName={styles.navigation}
-        navigationSelectedClassName={styles.selected}
-        navigationNotSelectedClassName={styles.notSelected}
-        items={events.map((event) => (
-          <img key={event.id} src={event.imageUrl} />
-        ))}
-      /> */}
       <Slider
         dots={true}
         infinite={true}
         speed={500}
         slidesToShow={1}
         slidesToScroll={1}
-        className={styles.innerWrapper}
+        dotsClass={`slick-dots ${styles.navigation}`}
       >
-        {events.map(
-          (event) => (
-            // <div key={item.index} className={`${styles.inner}`}>
-            <img key={event.id} src={event.imageUrl} />
-          ),
-          // </div>
-        )}
+        {events.map((event) => (
+          <img key={event.id} src={event.imageUrl} />
+        ))}
       </Slider>
     </div>
   );
