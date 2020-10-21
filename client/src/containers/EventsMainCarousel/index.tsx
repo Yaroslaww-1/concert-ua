d6b1094a8a97c96e72235315060dcb8f2da0cce5
Yaroslaww1 <yaroslaww1@gmail.com>
2020-10-21 20:53:50 +0300

    client: add arrow navigation to carousel

--- client/src/containers/EventsMainCarousel/index.tsx
+++ client/src/containers/EventsMainCarousel/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'src/redux/rootReducer';
 
 import styles from './styles.module.scss';
 import Slider from 'react-slick';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const EventsMainCarousel: React.FC = () => {
   const dispatch = useDispatch();
@@ -12,6 +15,29 @@ const EventsMainCarousel: React.FC = () => {
       state: { events },
     },
   } = useSelector((state: RootState) => ({ home: state.home }));
+
+  const NextArrow = (props: any) => {
+    const { className, style, onClick } = props;
+    return (
+      <div className={`${className} ${styles.arrow} ${styles.next}`} style={{ ...style }} onClick={onClick}>
+        <div className={`${styles.shadow} ${styles.next}`}>
+          <NavigateNextIcon className={`${styles.nextArrow}`} onClick={onClick} />
+        </div>
+      </div>
+    );
+  };
+
+  const PrevArrow = (props: any) => {
+    const { className, style, onClick } = props;
+    return (
+      <div className={`${className} ${styles.arrow} ${styles.prev}`} style={{ ...style }} onClick={onClick}>
+        <div className={`${styles.shadow} ${styles.prev}`}>
+          <NavigateBeforeIcon className={`${styles.prevArrow}`} onClick={onClick} />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.carouselWrapper}>
       <Slider
@@ -21,6 +47,8 @@ const EventsMainCarousel: React.FC = () => {
         slidesToShow={1}
         slidesToScroll={1}
         dotsClass={`slick-dots ${styles.navigation}`}
+        nextArrow={<NextArrow />}
+        prevArrow={<PrevArrow />}
       >
         {events.map((event) => (
           <img key={event.id} src={event.imageUrl} />
