01594b5e7a25542e7f0a288df373b1dcc757e284
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-05 11:29:40 +0200

    client: add style selection in TransparentButton

--- client/src/components/Buttons/TransparentButton/index.tsx
+++ client/src/components/Buttons/TransparentButton/index.tsx
@@ -4,12 +4,30 @@ import styles from './styles.module.scss';
 
 interface IProps {
   text: string;
+  color?: 'white' | 'red';
+  animationType?: 'appearing' | 'growCenter';
   onClick: () => void;
 }
 
-const TransparentButton: React.FC<IProps> = ({ text, onClick }) => {
+const TransparentButton: React.FC<IProps> = ({ text, onClick, color = 'white', animationType = 'appearing' }) => {
+  const getColorClass = () => {
+    const options = {
+      white: styles.white,
+      red: styles.red,
+    };
+    return options[color];
+  };
+
+  const getAnimationClass = () => {
+    const options = {
+      appearing: styles.appearing,
+      growCenter: styles.growCenter,
+    };
+    return options[animationType];
+  };
+
   return (
-    <div className={styles.root} onClick={onClick}>
+    <div className={`${styles.root} ${getColorClass()} ${getAnimationClass()}`} onClick={onClick}>
       <h1>{text}</h1>
     </div>
   );
