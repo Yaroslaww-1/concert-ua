f32b80492e92cab02c3bd2b8b878f4e32f2242a6
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-05 11:33:53 +0200

    client: start working on new events section

---
+++ client/src/containers/NewEvents/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { EventModel } from 'src/api/models/event.model';
+
+import styles from './styles.module.scss';
+import EventsCardsSection from 'src/components/Sections/EventCardsSection';
+
+const NewEventsSection: React.FC = () => {
+  const event: EventModel = {
+    id: 'string',
+    name: 'stringstringstringstringstring',
+    description: 'string',
+    place: 'string',
+    date: 'string',
+    imageUrl: 'https://storage.concert.ua/Jnt/15/jj/5f8807e64ec54/eca7.jpg:31-mainpage-megabanner-desktop',
+    price: '100$',
+  };
+  return <EventsCardsSection events={[event]} header={'New events'} onLoadMore={() => {}} />;
+};
+
+export default NewEventsSection;
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import styles from './styles.module.scss';
 import PageComponent from 'src/components/Page';
 import EventsMainCarousel from 'src/containers/EventsMainCarousel';
 import EventsSecondaryCarousel from 'src/containers/EventsSecondaryCarousel';
+import NewEventsSection from 'src/containers/NewEvents';
 
 const HomePage: React.FC = () => {
   const dispatch = useDispatch();
@@ -16,6 +17,7 @@ const HomePage: React.FC = () => {
     <PageComponent loading={false}>
       <EventsMainCarousel />
       <EventsSecondaryCarousel />
+      <NewEventsSection />
     </PageComponent>
   );
 };
