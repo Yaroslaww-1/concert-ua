e666c57604ed6a5bbd04fa75ef862ca65de3b7fe
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-10 12:01:55 +0200

    client: add page name

--- client/src/pages/Concerts/index.tsx
+++ client/src/pages/Concerts/index.tsx
@@ -4,10 +4,12 @@ import PageComponent from 'src/components/Page';
 import Subscribe from 'src/components/Subscribe';
 import Footer from 'src/components/Footer';
 import Filter from './containers/Filter';
+import PageNameSection from './components/PageNameSection';
 
 const Concerts: React.FC = () => {
   return (
     <PageComponent>
+      <PageNameSection />
       <Filter />
       <Subscribe onSubscribe={() => {}} />
       <Footer />
