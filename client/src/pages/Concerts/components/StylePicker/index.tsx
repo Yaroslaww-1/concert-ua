252600d17c7c3b6784aae5fd8d7a454d18c7d9f9
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-10 14:05:17 +0200

    client: move checkbox to separate component

--- client/src/pages/Concerts/components/StylePicker/index.tsx
+++ client/src/pages/Concerts/components/StylePicker/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import cssStyles from './styles.module.scss';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+
 import { StyleModel } from 'src/api/models/style.model';
-import Text from 'src/components/Text';
 import BorderlessTransparentButton from 'src/components/Buttons/BorderlessTransparentButton';
 import ColoredButton from 'src/components/Buttons/ColoredButton';
+import CheckboxList from 'src/components/CheckboxList';
 
 interface IProps {
   styles: StyleModel[];
@@ -19,12 +17,8 @@ interface IProps {
 const StylePicker: React.FC<IProps> = ({ styles, preSelected, onClose, onSelect }) => {
   const [selectedStyles, setSelectedStyles] = React.useState<StyleModel[]>(preSelected);
 
-  const isStyleSelected = (style: StyleModel) => {
-    return selectedStyles.some((_style) => _style.id === style.id);
-  };
-
-  const onToggle = (style: StyleModel) => {
-    if (!isStyleSelected(style)) {
+  const onToggle = (style: StyleModel, action: 'select' | 'unselect') => {
+    if (action === 'select') {
       setSelectedStyles([...selectedStyles, style]);
     } else {
       setSelectedStyles(selectedStyles.filter((_style) => _style.id !== style.id));
@@ -38,34 +32,12 @@ const StylePicker: React.FC<IProps> = ({ styles, preSelected, onClose, onSelect 
 
   return (
     <>
-      <FormGroup classes={{ root: cssStyles.root }}>
-        {styles.map((style) => (
-          <FormControlLabel
-            key={style.id}
-            control={
-              <Checkbox
-                className={cssStyles.checkboxRoot}
-                disableRipple
-                color="default"
-                checkedIcon={<span className={`${cssStyles.icon} ${cssStyles.checkedIcon}`} />}
-                icon={<span className={cssStyles.icon} />}
-                inputProps={{ 'aria-label': 'decorative checkbox' }}
-                onChange={() => onToggle(style)}
-              />
-            }
-            label={
-              <Text
-                textTransform="capitalize"
-                color="black"
-                classes={{ root: cssStyles.styleNameText }}
-                lineHeight={1.3}
-              >
-                {style.name}
-              </Text>
-            }
-          />
-        ))}
-      </FormGroup>
+      <CheckboxList<StyleModel>
+        items={styles}
+        preSelected={preSelected}
+        onToggle={onToggle}
+        classes={{ root: cssStyles.root }}
+      />
       <div className={cssStyles.confirmingButtons}>
         <BorderlessTransparentButton onClick={onClose} text="Cancel" color="red" />
         <ColoredButton onClick={onSubmitSelect} text="Submit" />
