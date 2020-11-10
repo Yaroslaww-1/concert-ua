f272356fe351d5752cd4ec6412795db9a15613cd
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-10 13:41:47 +0200

    client: add confirmation buttons to StylePicker

--- client/src/pages/Concerts/components/StylePicker/index.tsx
+++ client/src/pages/Concerts/components/StylePicker/index.tsx
@@ -6,6 +6,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import { StyleModel } from 'src/api/models/style.model';
 import Text from 'src/components/Text';
+import BorderlessTransparentButton from 'src/components/Buttons/BorderlessTransparentButton';
+import ColoredButton from 'src/components/Buttons/ColoredButton';
 
 interface IProps {
   styles: StyleModel[];
@@ -14,7 +16,7 @@ interface IProps {
   onSelect: (styles: StyleModel[]) => void;
 }
 
-const StylePicker: React.FC<IProps> = ({ styles, preSelected, onClose }) => {
+const StylePicker: React.FC<IProps> = ({ styles, preSelected, onClose, onSelect }) => {
   const [selectedStyles, setSelectedStyles] = React.useState<StyleModel[]>(preSelected);
 
   const isStyleSelected = (style: StyleModel) => {
@@ -29,29 +31,46 @@ const StylePicker: React.FC<IProps> = ({ styles, preSelected, onClose }) => {
     }
   };
 
+  const onSubmitSelect = () => {
+    onSelect(selectedStyles);
+    onClose();
+  };
+
   return (
-    <FormGroup classes={{ root: cssStyles.root }}>
-      {styles.map((style) => (
-        <FormControlLabel
-          key={style.id}
-          control={
-            <Checkbox
-              className={cssStyles.checkboxRoot}
-              disableRipple
-              color="default"
-              checkedIcon={<span className={`${cssStyles.icon} ${cssStyles.checkedIcon}`} />}
-              icon={<span className={cssStyles.icon} />}
-              inputProps={{ 'aria-label': 'decorative checkbox' }}
-            />
-          }
-          label={
-            <Text textTransform="capitalize" color="black" classes={{ root: cssStyles.styleNameText }} lineHeight={1.3}>
-              {style.name}
-            </Text>
-          }
-        />
-      ))}
-    </FormGroup>
+    <>
+      <FormGroup classes={{ root: cssStyles.root }}>
+        {styles.map((style) => (
+          <FormControlLabel
+            key={style.id}
+            control={
+              <Checkbox
+                className={cssStyles.checkboxRoot}
+                disableRipple
+                color="default"
+                checkedIcon={<span className={`${cssStyles.icon} ${cssStyles.checkedIcon}`} />}
+                icon={<span className={cssStyles.icon} />}
+                inputProps={{ 'aria-label': 'decorative checkbox' }}
+                onChange={() => onToggle(style)}
+              />
+            }
+            label={
+              <Text
+                textTransform="capitalize"
+                color="black"
+                classes={{ root: cssStyles.styleNameText }}
+                lineHeight={1.3}
+              >
+                {style.name}
+              </Text>
+            }
+          />
+        ))}
+      </FormGroup>
+      <div className={cssStyles.confirmingButtons}>
+        <BorderlessTransparentButton onClick={onClose} text="Cancel" color="red" />
+        <ColoredButton onClick={onSubmitSelect} text="Submit" />
+      </div>
+    </>
   );
 };
 
