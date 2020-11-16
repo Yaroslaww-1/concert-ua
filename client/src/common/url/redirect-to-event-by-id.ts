de13bc48a7089e3828c3418f875b043b7b214fa8
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-16 23:49:54 +0200

    client: add deploy to gh

--- client/src/common/url/redirect-to-event-by-id.ts
+++ client/src/common/url/redirect-to-event-by-id.ts
@@ -2,5 +2,6 @@ import { Routes } from 'src/common/enum/routes';
 
 export const redirectToEvent = (eventId: string) => {
   const eventPath = Routes.EVENT.split('/:id')[0];
-  window.location.href = `${eventPath}/${eventId}`;
+  window.location.href = `#${eventPath}/${eventId}`;
+  window.location.reload();
 };
