e2d043d1d5bf99aa57d9594dcc63e9a9c06366bb
Yaroslaww1 <yaroslaww1@gmail.com>
2020-11-16 23:08:09 +0200

    Updates

---
+++ precache-manifest.83c74656e56e83466a08d8e8a4141270.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf7f38aea6ccf8cbebccd27eb1ea2a4",
+    "url": "/concert-ua/index.html"
+  },
+  {
+    "revision": "ede18f900f6ee7a64e74",
+    "url": "/concert-ua/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "e05ba9e11eb876590424",
+    "url": "/concert-ua/static/css/main.6ae6454d.chunk.css"
+  },
+  {
+    "revision": "ede18f900f6ee7a64e74",
+    "url": "/concert-ua/static/js/2.e20fb66d.chunk.js"
+  },
+  {
+    "revision": "4f28afabc50d1569baab9f9abbd7aa36",
+    "url": "/concert-ua/static/js/2.e20fb66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05ba9e11eb876590424",
+    "url": "/concert-ua/static/js/main.ef458aca.chunk.js"
+  },
+  {
+    "revision": "20010e0381ab867f24da",
+    "url": "/concert-ua/static/js/runtime-main.49a8fb8b.js"
+  },
+  {
+    "revision": "13e3d0559f9d1376e89cd08198ad5f70",
+    "url": "/concert-ua/static/media/latoblack.13e3d055.ttf"
+  },
+  {
+    "revision": "5b2b2bea8002c5ac1bb02d60f5faddc1",
+    "url": "/concert-ua/static/media/latoblack.5b2b2bea.woff"
+  },
+  {
+    "revision": "67a4543adcb77e4c7796a6844fa188b7",
+    "url": "/concert-ua/static/media/latoblack.67a4543a.woff2"
+  },
+  {
+    "revision": "46380352796e5966f0d0991c91eec0ca",
+    "url": "/concert-ua/static/media/latoblackitalic.46380352.woff"
+  },
+  {
+    "revision": "8d3da84773c91de2fdd59fc993c1d570",
+    "url": "/concert-ua/static/media/latoblackitalic.8d3da847.ttf"
+  },
+  {
+    "revision": "a0894cb3424a7279b441a7edc7a60565",
+    "url": "/concert-ua/static/media/latoblackitalic.a0894cb3.woff2"
+  },
+  {
+    "revision": "2b4a86f012402080bad3e1334adb21d6",
+    "url": "/concert-ua/static/media/latobold.2b4a86f0.ttf"
+  },
+  {
+    "revision": "7ff9916172d5bdd2dd14a28f890ec83a",
+    "url": "/concert-ua/static/media/latobold.7ff99161.woff"
+  },
+  {
+    "revision": "e1cbc7aaea1cb5f65dd77ffdc5126556",
+    "url": "/concert-ua/static/media/latobold.e1cbc7aa.woff2"
+  },
+  {
+    "revision": "0e3cc35ec935cd8df38489d3c4fe138f",
+    "url": "/concert-ua/static/media/latobolditalic.0e3cc35e.woff2"
+  },
+  {
+    "revision": "340502a7461541ad879494bbab74cb0b",
+    "url": "/concert-ua/static/media/latobolditalic.340502a7.ttf"
+  },
+  {
+    "revision": "432c5130ceb6e5aee63661ed6032f6af",
+    "url": "/concert-ua/static/media/latobolditalic.432c5130.woff"
+  },
+  {
+    "revision": "4eeb1666fa62ad4b00ed957fe217739f",
+    "url": "/concert-ua/static/media/latohairline.4eeb1666.woff"
+  },
+  {
+    "revision": "acd7be8835e508b6f943093a76ce20ab",
+    "url": "/concert-ua/static/media/latohairline.acd7be88.woff2"
+  },
+  {
+    "revision": "d2665866f8f2ad38d9b29f88b6ad3cdd",
+    "url": "/concert-ua/static/media/latohairline.d2665866.ttf"
+  },
+  {
+    "revision": "8efde0150a3421cd492c0690eb3aeabc",
+    "url": "/concert-ua/static/media/latohairlineitalic.8efde015.woff2"
+  },
+  {
+    "revision": "b30788d8bc948bdc07a47017b3b66e5c",
+    "url": "/concert-ua/static/media/latohairlineitalic.b30788d8.ttf"
+  },
+  {
+    "revision": "ea87665a2c556a1c0d4a74220b9823ed",
+    "url": "/concert-ua/static/media/latohairlineitalic.ea87665a.woff"
+  },
+  {
+    "revision": "37bbdbcbc1b26577f0a009fdf34f060a",
+    "url": "/concert-ua/static/media/latoheavy.37bbdbcb.woff2"
+  },
+  {
+    "revision": "3c8e2fea9560cc782d239c52c81faeb0",
+    "url": "/concert-ua/static/media/latoheavy.3c8e2fea.woff"
+  },
+  {
+    "revision": "51e5d23a8b7586d11bc2112ad4201797",
+    "url": "/concert-ua/static/media/latoheavy.51e5d23a.ttf"
+  },
+  {
+    "revision": "915671b2e7a29a4deb2731a569b89d46",
+    "url": "/concert-ua/static/media/latoheavyitalic.915671b2.woff2"
+  },
+  {
+    "revision": "ac87c14fbb1e90314710450c81d1f138",
+    "url": "/concert-ua/static/media/latoheavyitalic.ac87c14f.ttf"
+  },
+  {
+    "revision": "d46e3712225f8f18b25f18bf90b755fd",
+    "url": "/concert-ua/static/media/latoheavyitalic.d46e3712.woff"
+  },
+  {
+    "revision": "4b1810180974f499643b285cc9b5aa59",
+    "url": "/concert-ua/static/media/latoitalic.4b181018.woff2"
+  },
+  {
+    "revision": "4d1864b975844cfe5590a6afa067d380",
+    "url": "/concert-ua/static/media/latoitalic.4d1864b9.ttf"
+  },
+  {
+    "revision": "62aeca010211d9c6c778ef256937a2bc",
+    "url": "/concert-ua/static/media/latoitalic.62aeca01.woff"
+  },
+  {
+    "revision": "4cd8101f3a8e452991f3b823c038a823",
+    "url": "/concert-ua/static/media/latolight.4cd8101f.ttf"
+  },
+  {
+    "revision": "caf520060ff4926b46bb58d2966f1dfd",
+    "url": "/concert-ua/static/media/latolight.caf52006.woff2"
+  },
+  {
+    "revision": "f7c88895b1d589f8dee785f727078c5b",
+    "url": "/concert-ua/static/media/latolight.f7c88895.woff"
+  },
+  {
+    "revision": "553439f3e94e00c9f908778a349b3144",
+    "url": "/concert-ua/static/media/latolightitalic.553439f3.ttf"
+  },
+  {
+    "revision": "b6a98825168d2514fbf4d8554cc7f334",
+    "url": "/concert-ua/static/media/latolightitalic.b6a98825.woff2"
+  },
+  {
+    "revision": "d412d55aa73a73d3be5af58ac7762823",
+    "url": "/concert-ua/static/media/latolightitalic.d412d55a.woff"
+  },
+  {
+    "revision": "4a6fa802c6fc2c12b4d2b79d291350be",
+    "url": "/concert-ua/static/media/latomedium.4a6fa802.woff2"
+  },
+  {
+    "revision": "7fd6bc6bdb0265ed41f84042c916c1d8",
+    "url": "/concert-ua/static/media/latomedium.7fd6bc6b.woff"
+  },
+  {
+    "revision": "c15fd21b2de268ed039c45198bdfaf4d",
+    "url": "/concert-ua/static/media/latomedium.c15fd21b.ttf"
+  },
+  {
+    "revision": "2cf1be2fd8cbf07de036b4e0ab14a377",
+    "url": "/concert-ua/static/media/latomediumitalic.2cf1be2f.woff"
+  },
+  {
+    "revision": "a021c82f9d2a87f5fd656311d25baa48",
+    "url": "/concert-ua/static/media/latomediumitalic.a021c82f.woff2"
+  },
+  {
+    "revision": "d910f1b9310942d9a3dc169be6f44c61",
+    "url": "/concert-ua/static/media/latomediumitalic.d910f1b9.ttf"
+  },
+  {
+    "revision": "03fcc2b689dbbde1994eebd1ea7de296",
+    "url": "/concert-ua/static/media/latoregular.03fcc2b6.ttf"
+  },
+  {
+    "revision": "1fbf86c9bbb08fd76eb28eb50eeeb86a",
+    "url": "/concert-ua/static/media/latoregular.1fbf86c9.woff"
+  },
+  {
+    "revision": "8201bf32d4c0cf86a54e75b4464caec6",
+    "url": "/concert-ua/static/media/latoregular.8201bf32.woff2"
+  },
+  {
+    "revision": "44aba5a619630ad15e3b85af5072368f",
+    "url": "/concert-ua/static/media/latosemibold.44aba5a6.woff2"
+  },
+  {
+    "revision": "9ab55ec42e1a85034ef71af1be4ce8b3",
+    "url": "/concert-ua/static/media/latosemibold.9ab55ec4.woff"
+  },
+  {
+    "revision": "ea3fcf1502443dcf3a66a76075c480ce",
+    "url": "/concert-ua/static/media/latosemibold.ea3fcf15.ttf"
+  },
+  {
+    "revision": "086d934b37c8392dd04c3d811aa43903",
+    "url": "/concert-ua/static/media/latosemibolditalic.086d934b.ttf"
+  },
+  {
+    "revision": "215d9916804f9d42f732a14f04c9b602",
+    "url": "/concert-ua/static/media/latosemibolditalic.215d9916.woff2"
+  },
+  {
+    "revision": "f6f0cb8c6fd46013cdf059fd14f431b7",
+    "url": "/concert-ua/static/media/latosemibolditalic.f6f0cb8c.woff"
+  },
+  {
+    "revision": "46c2ac5b83274f1b94033afb603fcb8a",
+    "url": "/concert-ua/static/media/latothin.46c2ac5b.woff"
+  },
+  {
+    "revision": "8da5272ad9e5f4f41eeec9069d3d85d8",
+    "url": "/concert-ua/static/media/latothin.8da5272a.ttf"
+  },
+  {
+    "revision": "b052ca2329967f94e4021902d973cd55",
+    "url": "/concert-ua/static/media/latothin.b052ca23.woff2"
+  },
+  {
+    "revision": "7c82ddc3b9296c4db2beeb9664d9928a",
+    "url": "/concert-ua/static/media/latothinitalic.7c82ddc3.woff2"
+  },
+  {
+    "revision": "94032ce5267d4fbeb40e68e70bc62a7a",
+    "url": "/concert-ua/static/media/latothinitalic.94032ce5.woff"
+  },
+  {
+    "revision": "c49607ee08bc00f53350875f18289c33",
+    "url": "/concert-ua/static/media/latothinitalic.c49607ee.ttf"
+  },
+  {
+    "revision": "7529563c817664387a06199b534895e4",
+    "url": "/concert-ua/static/media/leaguegothic.7529563c.ttf"
+  },
+  {
+    "revision": "896480ae68f3c9c4f09e57f449c80b16",
+    "url": "/concert-ua/static/media/leaguegothic.896480ae.woff"
+  },
+  {
+    "revision": "e00360035e6c44435a2dd93daef0ea38",
+    "url": "/concert-ua/static/media/leaguegothic.e0036003.woff2"
+  },
+  {
+    "revision": "0bd37246e937715166a30065b28b7f95",
+    "url": "/concert-ua/static/media/mastercard-logo.0bd37246.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/concert-ua/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/concert-ua/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/concert-ua/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/concert-ua/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "17821423d54da9c646bbab2cf1e5758e",
+    "url": "/concert-ua/static/media/visa-logo.17821423.svg"
+  }
+]);
