deb0389b652ce4e7564932f6b86c3ebbd555a04b
Yaroslaww1 <yaroslaww1@gmail.com>
2020-12-07 22:42:54 +0200

    client: make artists page adaptive

--- client/src/pages/Artists/components/Artists/index.tsx
+++ client/src/pages/Artists/components/Artists/index.tsx
@@ -8,19 +8,23 @@ import Text from 'src/components/Text';
 import SquareImageHoverOverlay from 'src/components/EventImageComponents/HoverOverlays/SquareImageHover';
 import { redirectToArtist } from 'src/common/url/redirect-to-artist-by-id';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { useIsDesktop } from 'src/common/hooks/media-hooks';
 
 type IProps = {
   artist: ArtistModel;
 } & RouteComponentProps;
 
 const Artist: React.FC<IProps> = ({ artist, history }) => {
+  const isDesktop = useIsDesktop();
+  const artistNameFontSize = isDesktop ? '7rem' : '5rem';
+
   return (
     <SquareImage
       imageSrc={artist.imageUrl}
       classes={{ root: styles.imageRoot }}
       frontElement={
         <div className={styles.artistName}>
-          <Text fontSize="7rem" textTransform="uppercase" fontFamily="League Gothic">
+          <Text fontSize={artistNameFontSize} textTransform="uppercase" fontFamily="League Gothic">
             {artist.name}
           </Text>
         </div>
