ac8c7668efdced9e96698d4a5ef77853325869ea
Yaroslaww1 <yaroslaww1@gmail.com>
2020-12-07 21:29:37 +0200

    client: add redirection from login/register pages to home page

--- client/src/pages/LoginRegister/components/LogoHeader/index.tsx
+++ client/src/pages/LoginRegister/components/LogoHeader/index.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 import Text from 'src/components/Text';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { Routes } from 'src/common/enum/routes';
 
-const LogoHeader: React.FC = () => {
+const LogoHeader: React.FC<RouteComponentProps> = ({ history }) => {
   return (
     <div className={styles.root}>
-      <Text color="red" fontSize="3rem" textTransform="uppercase" letterSpacing="1px" fontWeight={900}>
+      <Text
+        color="red"
+        fontSize="3rem"
+        textTransform="uppercase"
+        letterSpacing="1px"
+        fontWeight={900}
+        onClick={() => history.push(Routes.DEFAULT)}
+      >
         Music.ua
       </Text>
     </div>
   );
 };
 
-export default LogoHeader;
+export default withRouter(LogoHeader);
